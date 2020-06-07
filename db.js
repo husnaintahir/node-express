4b6bb48a379f1d80759b50ffdefed4c06fd45ab2
Husnain Tahir <husnaintahir@gmail.com>
2020-06-07 15:30:49 +0500

    db connection

--- db.js
+++ db.js
@@ -1,18 +1,28 @@
 const mongoose = require('mongoose');
 
 
-const MONGO_USERNAME = 'husnain';
-const MONGO_PASSWORD = '123';
-const MONGO_HOSTNAME = '192.168.1.200';
+const { db_host, db_password, db_user } = require('./config');
+
+const MONGO_USERNAME = db_user;
+const MONGO_PASSWORD = db_password;
+const MONGO_HOSTNAME = db_host;
 const MONGO_PORT = '27017';
-// const MONGO_PORT = '35182';
-const MONGO_DB = 'sharkinfo';
 
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const MONGO_DB = 'lofo';
+// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
 
-mongoose.connect(url, { useNewUrlParser: true }).then(() => {
+
+const MONGO_URL = `mongodb://${MONGO_HOSTNAME}/?authSource=admin`;
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    auth: {
+        user: MONGO_USERNAME,
+        password: MONGO_PASSWORD
+    }
+}).then(() => {
     console.log("Connected to Database");
 }).catch((err) => {
     console.log("Not Connected to Database ERROR! ", err);
